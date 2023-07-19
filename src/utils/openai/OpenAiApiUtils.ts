439883d3c91da0c8083f1c08d59d8556e8a919df
Jabuf <jason.buffet@gmail.com>
2023-07-19 10:45:58 +0200

    add openai and vitest packages

--- src/utils/openai/OpenAiApiUtils.ts
+++ src/utils/openai/OpenAiApiUtils.ts
@@ -3,10 +3,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export default abstract class ApiUtils {
-  private configuration = new Configuration({
+export default abstract class OpenAiApiUtils {
+  private static configuration = new Configuration({
     organization: process.env.OPENAI_ORGANIZATION_ID,
     apiKey: process.env.OPENAI_API_KEY,
   })
   private static openai = new OpenAIApi(this.configuration)
+
+  static async getEnginesList() {
+    return this.openai.listEngines()
+  }
 }
