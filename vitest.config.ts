882ba59b2520da9e29997ed82cbe298f311da98f
Jabuf <jason.buffet@gmail.com>
2023-11-29 12:08:16 +0100

    increase timeout for tu

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     exclude: [...configDefaults.exclude],
     // Options can be added here if needed : https://vitest.dev/config/
     globals: true,
-    testTimeout: 20000,
+    testTimeout: 200000,
   },
 })
