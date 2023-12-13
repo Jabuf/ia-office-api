fc3a399b17268fe1dfeb6d702a828474fcf3a3c8
Jabuf <jason.buffet@gmail.com>
2023-12-13 15:45:37 +0100

    store google secrets in env variables for now

--- src/utils/google/GoogleApiUtils.ts
+++ src/utils/google/GoogleApiUtils.ts
@@ -1,13 +1,16 @@
 import { JWT } from 'google-auth-library'
-import keys from './ia-office-keys.json'
+import dotenv from 'dotenv'
 
+dotenv.config()
 export default abstract class GoogleApiUtils {
   protected static client = new JWT({
-    email: keys.client_email,
-    key: keys.private_key,
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
     scopes: [
-      'https://www.googleapis.com/auth/spreadsheets',
       'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/documents',
+      'https://www.googleapis.com/auth/presentations',
+      'https://www.googleapis.com/auth/spreadsheets',
     ],
   })
 }
