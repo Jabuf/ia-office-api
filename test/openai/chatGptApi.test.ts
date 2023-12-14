186212bdfa0dba5dd5a26788c91e2f8c12af0b36
Jabuf <jason.buffet@gmail.com>
2023-12-14 12:02:32 +0100

    remove obsolete file

--- test/openai/chatGptApi.test.ts
+++
@@ -1,7 +0,0 @@
-import GptApiUtils from '../../src/utils/openai/GptApiUtils'
-import { expect } from 'vitest'
-
-it('Checking the API client', async () => {
-  const res = await GptApiUtils.startConv('Hello')
-  expect(res).toBeTypeOf('string')
-})
